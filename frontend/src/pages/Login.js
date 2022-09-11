bf4bb58538e283997c5956901a0e150762252cc9
lee <lsh3384@gmail.com>
2022-09-11 21:13:54 +0900

    modified login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,6 +2,10 @@ function Login() {
   return (
     <div>
       <p>로그인페이지</p>
+      <form method="POST" action="/loginAction">
+        <p><label>ID: <input key="id"/></label></p>
+        <p><label>PW: <input key="pw"/></label></p>
+      </form>
     </div>
   )
 }
