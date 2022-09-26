51252b66e51393952f89f0d50ab40f2ff161f78d
lee <lsh3384@gmail.com>
2022-09-26 23:38:30 +0900

    added reducer

--- frontend/src/modules/ducks.js
+++ frontend/src/modules/ducks.js
@@ -1,7 +1,24 @@
 // 액션 타입
 const LOGIN = "LOGIN";
-const LOGOUT = "LOGOUT";
+// const LOGOUT = "LOGOUT";
 
 // 액션 함수
-export const login = () => ( {type: LOGIN})
-export const logout = () => ( {type: LOGOUT})
+export const changeLogin = (status) => ( {type: LOGIN, })
+// export const logout = () => ( {type: LOGOUT})
+
+// 초기 상태
+const initialState = {
+    login: false
+}
+
+function login(state = initialState, action) {
+    switch (action.type) {
+        case LOGIN:
+            return {
+                ...state,
+                status: action.status,
+            }
+        default:
+            return state;
+    }
+}
