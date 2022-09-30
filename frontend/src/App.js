d381bbd71b565e86c6f93f6c4e21328dd5c7fbfe
lee <lsh3384@gmail.com>
2022-09-30 21:29:51 +0900

    added test page using ag-grid-react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Main from './pages/Main';
 import Login from './pages/Login';
 import Regist from './pages/Regist';
 import Post from './pages/Post';
+import Test from './pages/Test';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/login" element={<Login/>} />
       <Route path="/regist" element={<Regist/>} />
       <Route path="/post" element={<Post/>} />
+      <Route path="/test" element={<Test/>} />
     </Routes>
   );
 }
